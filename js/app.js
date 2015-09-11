ca0438c4deae6a78205b0b4f182e5dfe415649d9
Thomas Logan <tdloganj@gmail.com>
2015-09-11 11:26:24 -0700

    [feature] Search button now queries Twitch API using user's specified stream text.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
 //Here is where the main app logic will rest. It will take the query data and manipulate for use on the DOM
+
+var searchButton = document.getElementById("querySubmit");
+searchButton.addEventListener("click", getTwitchData, false);
--- js/queries.js
+++ js/queries.js
@@ -1,3 +1,25 @@
-// Here is where all of the TWITCH API query logic will be
+// // Here is where all of the TWITCH API query logic will be
 
 //Listen for Query Button Click event
+var getTwitchData = function() {
+  var queryText = document.getElementById("queryInput").value;
+  var twitchQuery = new XMLHttpRequest();
+
+  twitchQuery.open("GET", "https://api.twitch.tv/kraken/search/streams?q=" + queryText, true);
+  
+  twitchQuery.onload = function (e) {
+    //When readyState === 4, we know that the request has finished and the response is ready
+    if (twitchQuery.readyState === 4 && twitchQuery.status === 200) {
+      //We know we have a good request so we will parse the data
+      console.log(twitchQuery.status);
+      console.log(JSON.parse(twitchQuery.responseText));
+    } else {
+      //Otherwise we will log the error
+      console.error(twitchQuery.statusText);
+    }
+  };
+  
+  twitchQuery.send();
+}
+
+
