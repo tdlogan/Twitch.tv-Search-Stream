f8153ca18cb5261ba375eded0d0d70eb5499ced0
Thomas Logan <tdloganj@gmail.com>
2015-09-11 14:58:51 -0700

    [feature] Initial stream list loads when user submits a query. Still need to remove old streams on new search

--- js/queries.js
+++ js/queries.js
@@ -1,4 +1,6 @@
 // // Here is where all of the TWITCH API query logic will be
+var streamCount = document.getElementById("streamCount");
+var streamList = document.getElementById("listBody");
 
 //Listen for Query Button Click event
 var getTwitchData = function() {
@@ -10,11 +12,10 @@ var getTwitchData = function() {
   twitchQuery.onload = function (e) {
     //When readyState === 4, we know that the request has finished and the response is ready
     if (twitchQuery.readyState === 4 && twitchQuery.status === 200) {
-      //We know we have a good request so we will parse the data
-      console.log(twitchQuery.status);
       console.log(JSON.parse(twitchQuery.responseText));
+      setStreamCount(JSON.parse(twitchQuery.responseText));
+      loadStreamsList(JSON.parse(twitchQuery.responseText));
     } else {
-      //Otherwise we will log the error
       console.error(twitchQuery.statusText);
     }
   };
@@ -22,4 +23,45 @@ var getTwitchData = function() {
   twitchQuery.send();
 }
 
+
+// THIS WILL BE MOVED TO A UTILITIES FILE
+var setStreamCount = function(data) {
+  document.getElementById("streamCount").innerText = data._total;
+}
+
+var loadStreamsList = function(data) {
+  //We want to grab the 5 streams
+  for (var i = 0; i < 5; i++) {
+    appendStreamData(data.streams[i]);
+  }
+}
+
+var appendStreamData = function(data) {
+  var streamBlock = document.createElement("div");
+  streamBlock.classList.add("streamBlocks");
+  
+  //Appending images will be treated a little differently
+  var streamImage = document.createElement("img");
+  streamImage.src = data.preview.medium;
+  streamBlock.appendChild(streamImage);
+
+  var streamData = document.createElement("span");
+
+  appendingHelper(streamData, "h1", data.channel.display_name);
+  appendingHelper(streamData, "span", data.channel.game);
+  appendingHelper(streamData, "span", " - " + data.viewers + " viewers");
+  appendingHelper(streamData, "p", data.channel.status);
+
+  streamBlock.appendChild(streamData);
+
+  streamList.appendChild(streamBlock);
+
+}
+
+var appendingHelper = function(appendingElement, tagName, data) {
+  var newElement = document.createElement(tagName);
+  var tagData = document.createTextNode(data);
+  newElement.appendChild(tagData);
+  appendingElement.appendChild(newElement);
+}
 
