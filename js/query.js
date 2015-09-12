a44b28c18e095ded5477334df7a5d32fff1aa1b4
Thomas Logan <tdloganj@gmail.com>
2015-09-12 13:09:56 -0700

    [refactor] renamed the queries file to query.

---
+++ js/query.js
@@ -0,0 +1,27 @@
+var streamCount = $("#streamCount");
+var streamList = $("#listBody");
+var streamData = null;
+
+var getTwitchData = function(page) {
+  clearStreamList();
+  tempNavDisable();
+
+  var queryText = $("#queryInput").value;
+  var queryPath = streamData ? streamData._links[page] : "https://api.twitch.tv/kraken/search/streams?q=" + queryText;
+
+  var twitchQuery = new XMLHttpRequest();
+  twitchQuery.open("GET", queryPath, true);
+  twitchQuery.onload = function (e) {
+    if (twitchQuery.readyState === 4 && twitchQuery.status === 200) {
+      streamData = JSON.parse(twitchQuery.responseText);
+      setStreamCount(streamData);
+      setPageLimit(streamData);
+      loadStreamsList(streamData);
+      checkButtonAvailability();
+    } else {
+      console.error(twitchQuery.statusText);
+    }
+  };
+  
+  twitchQuery.send();
+};
